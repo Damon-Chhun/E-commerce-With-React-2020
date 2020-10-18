b832b5ae86af0928a07ea4399d4c05c54b344cfd
Damon Chhun <damonchhun7@gmail.com>
2020-10-18 10:31:35 -0700

    cart-items component

--- ecomm2020/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ ecomm2020/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import CartItems from "../cart-items/cart-items.component";
+import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/custom-buttom.component";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropDown = () => (
+const CartDropDown = ({ items }) => (
   <div className="cart-dropdown">
     <div className="cart-items" />
+    {items.map(items => (
+      <CartItems items={items} key={items.id} />
+    ))}
     <CustomButton>Go To Checkout!</CustomButton>
   </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = ({ cart }) => ({
+  items: cart.cartItems
+});
+
+export default connect(mapStateToProps)(CartDropDown);
