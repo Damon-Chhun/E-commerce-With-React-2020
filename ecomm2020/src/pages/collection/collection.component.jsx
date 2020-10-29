bc9f9dc76ed813c602f847602b5ee5bac17461f1
Damon Chhun <damonchhun7@gmail.com>
2020-10-29 07:36:38 -0700

    now displays all items on the collection component with styling applied

--- ecomm2020/src/pages/collection/collection.component.jsx
+++ ecomm2020/src/pages/collection/collection.component.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
 import { connect } from "react-redux";
 
-//import CollectionItem from "../../components/collection-item";
+import "./collection.styles.scss";
 
 const Collection = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
-    <div className="collection">
-      <h2>collection PAGE</h2>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
