92c366278658eaf4fc3f6179cfb2b191e3ac61bc
Damon Chhun <damonchhun7@gmail.com>
2020-10-30 20:14:46 -0700

    normilization of shop data:

--- ecomm2020/src/redux/shop/shop.selectors.js
+++ ecomm2020/src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_MAP_ID = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
@@ -15,9 +7,8 @@ export const selectShopCollections = createSelector(
   shop => shop.collections
 );
 
-export const selectCollection = collectionUrl =>
-  createSelector([selectShopCollections], collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_MAP_ID[collectionUrl]
-    )
+export const selectCollection = categoryID =>
+  createSelector(
+    [selectShopCollections],
+    collections => collections[categoryID]
   );
