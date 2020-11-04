3462edf8615c45a78a397ec7d1205736c6b79a9e
Damon Chhun <damonchhun7@gmail.com>
2020-11-04 06:58:18 -0800

    first styled component and fixes to organization of sass files

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { HomePageContainer } from "./homepage.styled";
 import "./Homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
-  <div className="homepage">
+  <div className="hompage">
     <Directory />
   </div>
 );
