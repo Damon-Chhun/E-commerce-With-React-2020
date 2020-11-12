a4ee6d58743660c1fd68f1203726195521d4568a
Damon Chhun <damonchhun7@gmail.com>
2020-11-12 11:15:19 -0800

    remove duplicated directory

--- ecomm2020/src/redux/user/user-selector.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-
-export const selectUser = state => state.user;
-
-export const selectCurrentUser = createSelector(
-  [selectUser],
-  users => users.currentUser
-);
