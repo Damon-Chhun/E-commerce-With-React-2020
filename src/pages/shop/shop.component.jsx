5ef3335fc72a6360ea77737d3c1873bb8e9ccbd9
Damon Chhun <damonchhun7@gmail.com>
2020-11-18 07:08:07 -0800

    creating subscribe and firestore snapshop functions to pull data from firestore into app

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,11 +3,31 @@ import CollectionOverview from "../../components/collection-overview/collection-
 import Collection from "../collection/collection.component";
 
 import { Route } from "react-router-dom";
+import { firestore } from "../../firebase/firebase.utils";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route exact path={`${match.path}/:categoryID`} component={Collection} />
-  </div>
-);
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshop = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+
+    collectionRef.onSnapshot(async snapshop => {
+      console.log(snapshop);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          exact
+          path={`${match.path}/:categoryID`}
+          component={Collection}
+        />
+      </div>
+    );
+  }
+}
 export default ShopPage;
