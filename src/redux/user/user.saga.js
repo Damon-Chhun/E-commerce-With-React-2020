59f72b0f1b8a555a0864909b0295c1dee6d318c9
Damon Chhun <damonchhun7@gmail.com>
2020-12-18 12:07:33 -0800

    setting pu for saga for google sign in

--- src/redux/user/user.saga.js
+++ src/redux/user/user.saga.js
@@ -1,1 +1,9 @@
 import { takeLatest, put } from "redux-saga/effects";
+
+import UserActionTypes from "./user.actiontypes";
+
+export function* signInWithGoogle() {}
+
+export function* googleSignInStart() {
+  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
+}
