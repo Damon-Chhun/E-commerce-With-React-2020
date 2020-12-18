aa160e4ad401e961cc9f55c616bb9c858f6fd133
Damon Chhun <damonchhun7@gmail.com>
2020-12-18 11:52:58 -0800

    new actions for sagas

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,45 @@
-import { UserActionTypes } from "./user.actiontypes";
+import UserActionTypes from "./user.actiontypes";
 
 export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user
 });
+
+export const googleSignInStart = () => {
+  return {
+    type: UserActionTypes.GOOGLE_SIGN_IN_START
+  };
+};
+
+export const googleSignInSuccess = user => {
+  return {
+    type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+    payload: user
+  };
+};
+
+export const googleSignInFail = error => {
+  return {
+    type: UserActionTypes.GOOGLE_SIGN_IN_FAIL,
+    data: error
+  };
+};
+
+export const emailSignInStart = emailPassword => {
+  return {
+    type: UserActionTypes.EMAIL_SIGN_IN_START,
+    payload: emailPassword
+  };
+};
+export const emailSignInSuccess = user => {
+  return {
+    type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+    payload: user
+  };
+};
+export const emailSignInFail = error => {
+  return {
+    type: UserActionTypes.EMAIL_SIGN_IN_FAIL,
+    payload: error
+  };
+};
